e7ff1ee6f4febb21334e35bb77e0158152dc0ace
ch3k0 <gufin80@gmail.com>
2017-03-10 13:35:58 -0600

    corrigiendo jsons en piecharts

---
+++ visualizaciones/piechart/js/dgm/cct_bcs.js
@@ -0,0 +1,81 @@
+
+  var jsonPiechart;
+  var total = 0;
+
+  $.ajax({
+    type: "GET",
+    url: "partials/cct_bcs.json",
+    async: false,
+    success: function(data){
+      if (validaJsonPieChart(data.valores)){
+          jsonPiechart = data;
+          jsonPiechart.valores.forEach(function(d) {
+            total = total + d.value;
+          });
+      }
+    }
+  });
+
+
+
+  d3plus.viz()
+  .container("#viz")
+  .data(jsonPiechart.valores)
+  .type("pie")
+  .id("ntipo")
+  .size("ntipo")
+  .color({
+    "heatmap": [ "#34dab3" , "#00cc99", "#158a8c" ],
+    "value": "value"
+  })
+  .legend(false)
+  .font({ "family": "'Open Sans', Helvetica, Arial, sans-serif" })
+  .format({
+    "text": function(text, params) {
+
+      if (text === "value") {
+        return jsonPiechart.unidad;
+      }
+      else {
+        return d3plus.string.title(text, params);
+      }
+
+    },
+    "number": function(number, params) {
+      n = parseFloat(number).toFixed(2)
+      var formatted = Number(n).toLocaleString('en');
+      if (params.key == "ntipo") {
+        //return "$" + formatted;
+        return formatted;
+      }
+      else {
+        return formatted + '%';
+      }
+    },
+    "locale":"es_ES"
+  })
+  .height(jsonPiechart.ancho)
+  .resize(true)
+  .draw()
+
+      // Validacion de Json
+      function validaJsonPieChart(json){
+          var json_fields = ['label', 'value'],
+              json_types = {'label': 'string', 'value': 'number'};
+
+          for(var elemento in json){
+              var llaves_elemento = Object.keys(json[elemento]);
+              for(var k in llaves_elemento ){
+                  if(!json_fields.some(elem => elem === llaves_elemento[k])){
+                      alert("Error en la estructura del JSON: Campo invalido: " + llaves_elemento[k]);
+                      return false;
+                  }
+
+                  if(typeof json[elemento][llaves_elemento[k]] !== json_types[llaves_elemento[k]]){
+                      alert("Error en la estructura del JSON: El campo " + llaves_elemento[k] + " debe ser de tipo " + json_types[llaves_elemento[k]]);
+                      return false;
+                  }
+              }
+          }
+          return true;
+      }

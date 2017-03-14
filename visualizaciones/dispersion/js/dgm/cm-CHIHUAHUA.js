ad64bf01ec170707cfa63cc47a9c65b3f72a2ed3
ch3k0 <gufin80@gmail.com>
2017-03-14 16:39:50 -0600

    correciones en concesiones mineras

---
+++ visualizaciones/dispersion/js/dgm/cm-CHIHUAHUA.js
@@ -0,0 +1,42 @@
+  d3plus.viz()
+      .container("#scatterplot")
+      .data("partials/cm-CHIHUAHUA.json")
+
+      .type("scatter")
+      .id(["municipio", "titular"])
+      .x({
+          "value": "superficie",
+          "scale": "linear",
+          "label": "Superficie en km²"
+      })
+      .y({
+          "value": "vigencia",
+          "stacked": "true",
+          "label": "Vigencia en meses"
+      })
+      .size("superficie")
+      .color({
+          "heatmap": ["#34DAB3", "#30D1AE", "#2DC8AA", "#29BFA6", "#26B6A1", "#22AD9D", "#1FA499", "#1B9B94", "#189290", "#158A8C"],
+          "value": "superficie"
+      })
+      .font({
+          "family": "'Open Sans', Helvetica, Arial, sans-serif",
+          "size": 11
+      })
+      .format({
+          "number": function(number, params) {
+              //var formattedFirst = d3plus.number.format(number, params);
+              var formatted = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+              n = parseFloat(number).toFixed(1);
+              var formatted = Number(n).toLocaleString('en');
+              return formatted;
+          },
+          "locale": "es_ES"
+      })
+      .labels(true)
+      .tooltip({
+          "value": ["titular", "titulo", "superficie", "lote", "inicio", "fin", "vigencia", "descripcion", "carta_inegi"],
+          "small": 400
+      })
+      .resize(true)
+      .draw()

e4972b7d62555399d72f749b024d0313e33b34fd
ch3k0 <gufin80@gmail.com>
2017-03-28 19:22:20 -0600

    composicion - oficinas issste

---
+++ visualizaciones/composicion/js/dgm/matriz-directorio-oficinas.js
@@ -0,0 +1,54 @@
+//instantiate d3plus Treemap
+var visualization = d3plus.viz()
+.container("#treemapd3") // container DIV to hold the visualization
+.data("partials/dgm/matriz-directorio-oficinas.json")
+.id(["entidad", "municipio","dependencia", "nombre"]) // key for which our data is unique on
+.type("tree_map") //visualization type
+.size("valor") //sizing of blocks
+//Rango de colores según valor
+.color({
+    "heatmap": ["#34dab3", "#00cc99", "#158a8c"],
+    "range": ["#34dab3", "#00cc99", "#158a8c"],
+    "value": "valor"
+})
+.font({
+    "family": "'Open Sans', Helvetica, Arial, sans-serif",
+    "size": 14
+})
+.format({
+    "text": function(text, params) {
+
+        if (text === "valor") {
+            return "Oficinas";
+        }
+        if (text === "dependencia") {
+            return text;
+        }
+        else {
+            return d3plus.string.title(text, params);
+        }
+    },
+    "number": function(number, params) {
+        var formattedFirst = d3plus.number.format(number, params);
+        //var formatted = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        n = parseFloat(number).toFixed(0)
+        var formatted = Number(n).toLocaleString('en');
+        if (params.key == "valor") {
+            return n;
+        }
+        else {
+            return formatted + '%';
+        }
+    },
+    "locale": "es_ES"
+})
+.tooltip({
+    "value": ["nombre", "telefono", "domicilio"],
+    "small": 350
+})
+.resize(true)
+.legend(false)
+.ui({
+    "padding": 15
+})
+.draw() //finally, draw the visualization!

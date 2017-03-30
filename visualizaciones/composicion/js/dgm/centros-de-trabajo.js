d0fc3de73be4bcf738188faac148b7c873ed2ef4
ch3k0 <gufin80@gmail.com>
2017-03-29 22:17:42 -0600

    catalago de centros de trabajo en composicion

---
+++ visualizaciones/composicion/js/dgm/centros-de-trabajo.js
@@ -0,0 +1,49 @@
+//instantiate d3plus Treemap
+var visualization = d3plus.viz()
+    .container("#treemapd3") // container DIV to hold the visualization
+    .data("partials/dgm/centros-de-trabajo.json")
+    .id(["entidad", "tipo", "nivel", "control", "ambito"]) // key for which our data is unique on
+    .type("tree_map") //visualization type
+    .size("conteo") //sizing of blocks
+    //Rango de colores según valor
+    .color({
+        "heatmap": ["#34dab3", "#00cc99", "#158a8c"],
+        "range": ["#34dab3", "#00cc99", "#158a8c"],
+        "value": "conteo"
+    })
+    .font({
+        "family": "'Open Sans', Helvetica, Arial, sans-serif",
+        "size": 14
+    })
+    .format({
+        "text": function(text, params) {
+
+            if (text === "conteo") {
+                return "Centros de Trabajo";
+            } else {
+                return d3plus.string.title(text, params);
+            }
+
+        },
+        "number": function(number, params) {
+            var formattedFirst = d3plus.number.format(number, params);
+            //var formatted = number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            n = parseFloat(number).toFixed(1)
+            var formatted = Number(n).toLocaleString('en');
+            if (params.key == "conteo") {
+                return formatted;
+            } else {
+                return formatted + '%';
+            }
+        },
+        "locale": "es_ES"
+    })
+    .tooltip({
+        "small": 350
+    })
+    .resize(true)
+    .legend(false)
+    .ui({
+        "padding": 15
+    })
+    .draw() //finally, draw the visualization!

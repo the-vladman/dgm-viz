ceba6ae4efd88783fade70a84b610d7011c34b27
ch3k0 <gufin80@gmail.com>
2017-04-17 10:38:03 -0500

    correcciones qa

--- visualizaciones/js/main.js
+++ visualizaciones/js/main.js
@@ -61,27 +61,23 @@ function getInfoChart(var1) {
             descripcion = "El Índice de Rezago Social (IRS) permite ordenar las entidades federativas y municipios  de mayor a menor grado de rezago social en un momento del tiempo. <br>Es una medida en la que un solo índice agrega variables de educación, de acceso a servicios de salud, de servicios básicos en la vivienda, de calidad y espacios en la misma, y de activos en el hogar. Es decir, proporciona el resumen de cuatro carencias sociales de la medición de pobreza del CONEVAL: rezago educativo, acceso a los servicios de salud, acceso a los servicios básicos en la vivienda y la calidad y espacios en la vivienda.";
             datos = ["Índice de Rezago Social,2000-2010 (nacional,estatal,municipal, localidad y ageb)", "CONEVAL", "CSV", "https://datos.gob.mx/busca/dataset/indice-de-rezago-social20002005-y-2010-nacionalestatalmunicipal-y-localidad", "http://www.coneval.org.mx/Medicion/IRS/Paginas/Que-es-el-indice-de-rezago-social.aspx"];
             break;
-        case (grafica.match(/Crecimiento.*/) || {}).input:
+        case (grafica.match(/Crecimiento Total Anual.*/) || {}).input:
             descripcion = "Conjunto de indicadores que muestran un panorama general del cambio en la estructura y dinámica de la población así como en las tendencias y niveles de los fenómenos demográficos en México y las entidades federativas, entre los que encontramos esperanzas de vida al nacer, tasas específicas de fecundidad, crecimiento total, nacimientos, entre otros.";
             datos = ["Proyecciones de la población de México", "CONAPO", "JSON", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
             break;
-        case (grafica.match(/Esperanza de vida.*/) || {}).input:
+        case (grafica.match(/Esperanza de vida al nacimiento total.*/) || {}).input:
             descripcion = "Conjunto de indicadores que muestran un panorama general del cambio en la estructura y dinámica de la población así como en las tendencias y niveles de los fenómenos demográficos en México y las entidades federativas, entre los que encontramos esperanzas de vida al nacer, tasas específicas de fecundidad, crecimiento total, nacimientos, entre otros.";
             datos = ["Proyecciones de la población de México", "CONAPO", "JSON", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
             break;
-        case (grafica.match(/Calidad general del agua.*/) || {}).input:
+        case (grafica.match(/^Calidad general del agua/) || {}).input:
             descripcion = "Contiene las principales variables para medir la calidad del agua por sitio, cuenca, estado. A su vez indica el tipo de sistema ambiental que se mide.";
             datos = ["Red Nacional de Monitoreo de la Calidad de las Aguas Nacionales", "CONAGUA", "XLS", "https://datos.gob.mx/busca/dataset/red-nacional-de-monitoreo-de-la-calidad-de-las-aguas-nacionales", "http://201.116.60.25/sina/index_jquery-mobile2.html?tema=calidadAgua"]
             break;
-        case (grafica.match(/Escuelas por Estado.*/) || {}).input:
+        case (grafica.match(/^Escuelas por Estado/) || {}).input:
             descripcion = "Contiene datos sobre escuelas a nivel estatal donde se incluye información respectiva sobre el nombre de la escuela, domicilio,  localización, tipo de sostenimiento de la escuela, así como nivel y tipo de educación.";
             datos = ["Matrícula por Institución y Entidad Federativa", "SEP", "CSV", "https://datos.gob.mx/busca/dataset/censo-de-escuelas-maestros-y-alumnos-de-educacion-basica-y-especial", "http://www.snie.sep.gob.mx/estadisticas_educativas.html "]
             break;
-        case (grafica.match(/Inscritos en RETC.*/) || {}).input:
-            descripcion = " La gráfica representa los porcentajes de empresas, industrias y organizaciones por estado que se encuentran inscritas en el RETC; base de datos nacional con información de sustancias contaminantes emitidas al ambiente: aire, agua, suelo y subsuelo o que son transferidas en el agua residual y/o en los residuos peligrosos.";
-            datos = ["Registro de Emisiones y Transferencia de Contaminantes", "SEMARNAT", "XLS", "https://datos.gob.mx/busca/dataset/registro-de-emisiones-y-transferencia-de-contaminantes", "http://www.semarnat.gob.mx/temas/gestion-ambiental/calidad-del-aire/registro-de-emisiones-y-transferencia-de-contaminantes-retc"]
-            break;
-        case (grafica.match(/Llegadas a hoteles.*/) || {}).input:
+        case (grafica.match(/^Llegadas a hoteles.*/) || {}).input:
             descripcion = "Con información del Sistema de Monitoreo DataTur, se reportan los principales resultados para las variables de las actividades de alojamiento en los principales destinos del país. Entre las principales variables que podrá consultar los cuartos disponibles, cuartos ocupados, porcentaje de ocupación, entre otras.";
             datos = ["Actividad Hotelera por Entidad Federativa", "SECTUR", "CSV", "https://datos.gob.mx/busca/dataset/actividad-hotelera-por-entidad-federativa", "http://www.datatur.sectur.gob.mx/SitePages/CompendioEstadistico.aspx"]
             break;
@@ -89,23 +85,23 @@ function getInfoChart(var1) {
             descripcion = "Gráfica de las localidades que cuentan con el nombramiento de Pueblo Mágico por entidad federativa.";
             datos = ["Localidades que cuentan con el nombramiento de Pueblo Mágico", "SECTUR", "XLS", "https://datos.gob.mx/busca/dataset/localidades-que-cuentan-con-el-nombramiento-de-pueblo-magico", "http://www.gob.mx/sectur/acciones-y-programas/programa-pueblos-magicos"]
             break;
-        case (grafica.match(/Población por estado.*/) || {}).input:
+        case (grafica.match(/^Población por estado.*/) || {}).input:
             descripcion = "Es una medida en la que un solo índice agrega variables de educación, de acceso a servicios de salud, de servicios básicos en la vivienda, de calidad y espacios en la misma, y de activos en el hogar. Es decir, proporciona el resumen de cuatro carencias sociales de la medición de pobreza del CONEVAL: rezago educativo, acceso a los servicios de salud, acceso a los servicios básicos en la vivienda y la calidad y espacios en la vivienda.";
             datos = ["Índice de Rezago Social,2000-2010 (nacional,estatal,municipal, localidad y ageb)", "CONEVAL", "CSV", "https://datos.gob.mx/busca/dataset/indice-de-rezago-social20002005-y-2010-nacionalestatalmunicipal-y-localidad", "http://www.coneval.gob.mx/Informes/Pobreza/Datos_abiertos/IRS/Rezago_Social_2010_localidades_DIC.txt"]
             break;
-        case (grafica.match(/Residuos solidos.*/) || {}).input:
+        case (grafica.match(/^Residuos solidos.*/) || {}).input:
             descripcion = "Tabla con total de puntos muestreados, % eficiencia de cloración, % cobertura de vigilancia, % población con cobertura de vigilancia, % muestras aptas para consumo, % dentro y...";
             datos = ["Calidad del agua de uso y consumo humano", "COFEPRIS", "XLS", "https://datos.gob.mx/busca/dataset/indicadores-urbanos"]
             break;
-        case (grafica.match(/Proyecciones de los hogares.*/) || {}).input:
+        case (grafica.match(/^Proyecciones de los hogares en México.*/) || {}).input:
             descripcion = "En esta sección se presenta la información sobre los resultados de las estimaciones demográficas para el 2010-2030 en cuanto a los hogares. Dicha información es necesaria y relevante para llevar a cabo la planeación demográfica, económica y social del país, al mismo tiempo que presenta una herramienta de conocimiento valiosa para estimar múltiples requerimientos futuros en servicios e infraestructura, así como otras necesidades sociales.";
             datos = ["Proyecciones de la población de México", "CONAPO", "CSV", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
             break;
-        case (grafica.match(/Proyecciones de los hogares índigenas.*/) || {}).input:
-            descripcion = "En esta sección se presenta la información sobre los resultados de las estimaciones demográficas para el 2010-2030 en cuanto a los hogares indígenas. Dicha información es necesaria y relevante para llevar a cabo la planeación demográfica, económica y social del país, al mismo tiempo que presenta una herramienta de conocimiento valiosa para estimar múltiples requerimientos futuros en servicios e infraestructura, así como otras necesidades sociales.";
-            datos = ["Proyecciones de la población de México", "CONAPO", "CSV", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/en/CONAPO/Proyecciones_de_Hogares_Indigenas_de_Mexico_y_las_Entidades_Federativas_2010-2020"]
+        case (grafica.match(/^Proyecciones de los hogares Indígenas de México/) || {}).input:
+            descripcion = "En esta sección se presenta la información sobre los datos, metodología y análisis de los resultados de las estimaciones demográficas de los hogares indígenas en México para el periodo 2010-2020  Dicha información es necesaria y relevante para llevar a cabo la planeación demográfica, económica y social del país, al mismo tiempo que presenta una herramienta de conocimiento valiosa para estimar múltiples requerimientos futuros en servicios e infraestructura, así como otras necesidades sociales.";
+            datos = ["Proyecciones de la población de México", "CONAPO", "CSV", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
             break;
-        case (grafica.match(/Tasa específica de fecundidad.*/) || {}).input:
+        case (grafica.match(/^Tasa específica de fecundidad.*/) || {}).input:
             descripcion = "Conjunto de indicadores que muestran un panorama general del cambio en la estructura y dinámica de la población así como en las tendencias y niveles de los fenómenos demográficos en México y las entidades federativas, entre los que encontramos esperanzas de vida al nacer, tasas específicas de fecundidad, crecimiento total, nacimientos, entre otros.";
             datos = ["Proyecciones de la población de México", "CONAPO", "JSON", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.gob.mx/conapo/documentos/dinamica-demografica-1990-2010-y-proyecciones-de-poblacion-2010-2030"]
             break;
@@ -121,11 +117,11 @@ function getInfoChart(var1) {
             descripcion = "Estimación de fallecimientos en la República Mexicana (1990-2050) y Entidades Federativas (1990-2030).";
             datos = ["Proyecciones de la población de México", "CONAPO", "JSON", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
             break;
-        case (grafica.match(/Por localidad.*/) || {}).input:
+        case (grafica.match(/^Crecimiento de la Población por Localidad/) || {}).input:
             descripcion = "Representa proyecciones de crecimiento de la población por localidad de 2010 a 2030.";
             datos = ["Proyecciones de la población de México", "CONAPO", "JSON", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/work/models/CONAPO/Proyecciones/Datos/Bases_de_Datos/Proyecciones_Localidades/Descriptor/Diccionario_Proyecciones_por_Localidad.csv", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
             break;
-        case (grafica.match(/Municipios 2010.*/) || {}).input:
+        case (grafica.match(/^Crecimiento de la Población por Municipios/) || {}).input:
             descripcion = "Representa proyecciones de crecimiento de la población por municipio de 2010 a 2030.";
             datos = ["Proyecciones de la población de México", "CONAPO", "JSON", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/work/models/CONAPO/Proyecciones/Datos/Bases_de_Datos/Proyecciones_Municipios/Descriptor/Diccionario_Proyecciones_por_Municipios.csv", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
             break;
@@ -145,23 +141,19 @@ function getInfoChart(var1) {
             descripcion = "Las concesiones mineras otorgan a sus titulares el derecho a explotar, explotar y beneficiar todas las sustancias concesibles que se localicen dentro del lote minero que amparen.";
             datos = ["Concesiones Mineras", "SE", "CSV", "https://datos.gob.mx/busca/dataset/concesiones-mineras", "http://www.siam.economia.gob.mx/work/models/siam/Resource/Avisos/Fund_basicos_conc_min.pdf"]
             break;
-        case (grafica.match(/Cultivos de tu Municipio 2015.*/) || {}).input:
+        case (grafica.match(/^Cultivos de tu Municipio 2015.*/) || {}).input:
             descripcion = "La gráfica de composición muestra la información cierre de los cultivos con mayor rendimiento a nivel estatal y municipal en 2015.";
             datos = ["Proveedores, Comercialización y Cultivos", "SAGARPA", "XLS", "https://datos.gob.mx/busca/dataset/proveedores-comercializacion-y-cultivos", "http://www.gob.mx/siap/acciones-y-programas/produccion-agricola-33119"]
             break;
-        case (grafica.match(/Proyecciones de los hogares Indígenas de México.*/) || {}).input:
-            descripcion = "En esta sección se presenta la información sobre los datos, metodología y análisis de los resultados de las estimaciones demográficas de los hogares indígenas en México para el periodo 2010-2020  Dicha información es necesaria y relevante para llevar a cabo la planeación demográfica, económica y social del país, al mismo tiempo que presenta una herramienta de conocimiento valiosa para estimar múltiples requerimientos futuros en servicios e infraestructura, así como otras necesidades sociales.";
-            datos = ["Proyecciones de la población de México", "CONAPO", "CSV", "https://datos.gob.mx/busca/dataset/proyecciones-de-la-poblacion-de-mexico", "http://www.conapo.gob.mx/es/CONAPO/Proyecciones"]
-            break;
-        case (grafica.match(/Catálogo de Centros de Trabajo por Entidad - .*/) || {}).input:
+        case (grafica.match(/^Catálogo de Centros de Trabajo por Entidad - .*/) || {}).input:
             descripcion = "En esta sección se presenta la información sobre los Centros escolares de Educación Básica, Media Superior, Superior, Especial, Inicial y Formación para el Trabajo con sus características básicas relativas a su situación geográfica y administrativa.";
             datos = ["Catálogo de Centros de Trabajo", "SEP", "XLSX", "https://datos.gob.mx/busca/dataset/catalogo-de-centros-de-trabajo", "http://www.sep.gob.mx/es/sep1/directorio_de_escuelas"]
             break;
-        case (grafica.match(/Transferencias a Entidades Federativas.*/) || {}).input:
+        case (grafica.match(/^Transferencias a Entidades Federativas.*/) || {}).input:
             descripcion = "La Secretaría de Hacienda y Crédito Público pone a disposición del público un conjunto de datos sobre indicadores fiscales como balances, ingresos, gastos, financiamiento y deuda pública del Gobierno Federal, entidades paraestatales no financieras, entidades paraestatales financieras, sector público presupuestario y sector público federal, así como transferencias federales a Estados y Municipios.";
             datos = ["Estadísticas Oportunas de Finanzas Públicas", "SHCP", "CSV", "https://datos.gob.mx/busca/dataset/estadisticas-oportunas-de-finanzas-publicas", "http://www.shcp.gob.mx/POLITICAFINANCIERA/FINANZASPUBLICAS/Estadisticas_Oportunas_Finanzas_Publicas/Paginas/unica2.aspx"]
             break;
-        case (grafica.match(/Ingreso, Gasto y Financiamiento.*/) || {}).input:
+        case (grafica.match(/^Ingreso, Gasto y Financiamiento.*/) || {}).input:
             descripcion = "La Secretaría de Hacienda y Crédito Público pone a disposición del público un conjunto de datos sobre indicadores fiscales como balances, ingresos, gastos, financiamiento y deuda pública del Gobierno Federal, entidades paraestatales no financieras, entidades paraestatales financieras, sector público presupuestario y sector público federal, así como transferencias federales a Estados y Municipios.";
             datos = ["Estadísticas Oportunas de Finanzas Públicas", "SHCP", "CSV", "https://datos.gob.mx/busca/dataset/estadisticas-oportunas-de-finanzas-publicas", "http://www.shcp.gob.mx/POLITICAFINANCIERA/FINANZASPUBLICAS/Estadisticas_Oportunas_Finanzas_Publicas/Paginas/unica2.aspx"]
             break;

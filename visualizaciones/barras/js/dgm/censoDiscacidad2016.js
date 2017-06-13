4fa4660a6b55bc04f017efe8ac147372ad811507
ch3k0 <gufin80@gmail.com>
2017-06-12 22:26:53 -0500

    add gráfica censo de personas con discapacidad 2016

---
+++ visualizaciones/barras/js/dgm/censoDiscacidad2016.js
@@ -0,0 +1,96 @@
+var jsonChart;
+  //Llamada ajax Json
+  $.ajax({
+      async: false,
+      type: "GET",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      url: "partials/censoDiscacidad2016.json",
+      success: function(data) {
+          jsonChart = data;
+      }
+  });
+  var dimension;
+  var valor = obtenerValorParametro("muestra");
+    if (valor == "td"){
+      $('#vermas').show();
+      $("#titulo").html("<strong><p>"+jsonChart.titulo+"</p></strong>");
+      $("#descripcion").html("<p>"+jsonChart.descripcion+"</p>");
+      dimension = window.innerHeight - $("#titulo").outerHeight(true) - $("#descripcion").outerHeight(true) - $("#vermas").outerHeight(true) - 50;
+    }
+    else if (valor == "t"){
+      $('#vermas').show();
+      $('#descripcion').remove();
+      $("#titulo").html("<strong><p>"+jsonChart.titulo+"</p></strong>");
+      dimension = window.innerHeight - $("#titulo").outerHeight(true) - $("#vermas").outerHeight(true) - 40;
+    }
+    else if (valor == "d"){
+      $('#vermas').show();
+      $('#titulo').remove();
+      $("#descripcion").html("<p>"+jsonChart.descripcion+"</p>");
+      dimension = window.innerHeight - $("#descripcion").outerHeight(true) - $("#vermas").outerHeight(true) - 40;
+    }
+    else{
+      $('#vermas').remove();
+      $('#titulo').remove();
+      $('#descripcion').remove();
+      dimension = window.innerHeight - 20;
+    }
+
+//función para leer los parametros pasados por medio de la url
+function obtenerValorParametro(sParametroNombre) {
+  var sPaginaURL = window.location.search.substring(1);
+   var sURLVariables = sPaginaURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+      var sParametro = sURLVariables[i].split('=');
+      if (sParametro[0] == sParametroNombre) {
+        return sParametro[1];
+      }
+    }
+   return null;
+}
+var visualization = d3plus.viz()
+.container("#viz") // container DIV to hold the visualization
+.data(jsonChart.datos)
+.id("sexo") // key for which our data is unique on
+.type("bar") //visualization type
+//Rango de colores según valor
+.color({
+  "heatmap": [ "#d4d481" , "#ffaf65", "#ec6d65" ],
+  "range": [ "#d4d481" , "#ffaf65", "#ec6d65" ],
+  "value": "discapacidad"
+})
+.x("entidad")
+.y("discapacidad")
+.order({"agg":"max","sort":"asc","value":"discapacidad"})
+.font({ "family": "'Open Sans', sans-serif", "size": 14 })
+.format({
+  "text": function(text, params) {
+    if (text === "h") {
+      return "Hombres";
+    }
+    if (text === "m") {
+      return "Mujeres";
+    }
+    if (text === "discapacidad") {
+      return "Personas con discapacidad";
+    }
+    if (text === "entidad") {
+      return "Entidades de México";
+    }
+    else {
+      return d3plus.string.title(text, params);
+    }
+  },
+  "number": function(number, params) {
+    var formattedFirst = d3plus.number.format(number, params);
+        n = parseFloat(number).toFixed(1);
+    formatted = Number(n).toLocaleString('en');
+    return formatted;
+  },
+  "locale":"es_ES"
+})
+.legend(false)
+.resize(true)
+.height(dimension)
+.draw()  //finally, draw the visualization!
